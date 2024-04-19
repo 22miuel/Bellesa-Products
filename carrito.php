d193dd469016b7528eb9dd15b26b812d32a06357
victorluis7100 <victor7100.rodriguez@gmail.com>
2024-04-19 11:29:52 -0400

    fix make the cart.php

--- carrito.php
+++ carrito.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 $mensaje="";
 if(isset($_POST['btnAccion'])){
     switch($_POST['btnAccion']){
@@ -14,8 +14,10 @@ if(isset($_POST['btnAccion'])){
 
             if(is_string(openssl_decrypt($_POST['nombre'],COD,KEY))){
                 $NOMBRE=openssl_decrypt($_POST['nombre'],COD,KEY);
-                $mensaje.="OK nombre".$NOMBRE."<br/>"
-            }else{$mensaje.="Upss.. algo pasa con el nombre"."<br/>"; break;}
+                $mensaje.="OK nombre".$NOMBRE."<br/>";
+        
+            }else{
+                $mensaje.="Upss.. algo pasa con el nombre"."<br/>"; break;}
 
             if(is_numeric(openssl_decrypt($_POST['cantidad'],COD,KEY))){
                 $CANTIDAD=openssl_decrypt($_POST['cantidad'],COD,KEY);
@@ -25,9 +27,47 @@ if(isset($_POST['btnAccion'])){
             if(is_numeric((openssl_decrypt($_POST['precio'],COD,KEY)))){
                 $PRECIO=openssl_decrypt($_POST['precio'],COD,KEY);
                 $mensaje.="OK precio".$PRECIO."<br/>";
-            }else{ $mensaje.="uppss.. algo pasa con el precio"."<br/>";break}
+            }else{ $mensaje.="uppss.. algo pasa con el precio"."<br/>";break;}
 
+            if(!isset($_SESSION['CARRITO'])){
+                $producto=array(
+                    'ID'=>$ID,
+                    'NOMBRE'=>$NOMBRE,
+                    'CANTIDAD'=>$CANTIDAD,
+                    'PRECIOS'=>$PRECIOS,
+                );
+$_SESSION['CARRITO'][0]=$producto;
+            }else{
+                $NumeroProductos=count($_SESSION['CARRITO']);
+                $producto=array(
+                    'ID'=>$ID,
+                    'NOMBRE'=>$NOMBRE,
+                    'CANTIDAD'=>$CANTIDAD,
+                    'PRECIOS'=>$PRECIOS,
+                );
+                $_SESSION['CARRITO'][$NumeroProductos]=$producto;
+            }
+            $mensaje=print_r($_SESSION,true);
             break;
+            case"Eliminar";
+            if(is_numeric( openssl_decrypt($_POST['id'],COD,KEY))){
+                $ID=openssl_decrypt($_POST['id'],COD,KEY);
+              foreach($_SESSION['CARRITO'] as $indice=>$producto){
+
+                if($producto['ID']===$ID){
+                    unset($_SESSION['CARRITO'][$indice]);
+                    echo "<script>alert('Elemento borrado...');</script>";
+
+                }
+              }
+
+            }else{
+                $mensaje.= "UPSS... ID incorrecto"."<br/>".$ID;
+            }
+            break;
+    
+        }
     }
-}
+    
+
 ?>
--- index.php
+++ index.php
@@ -2,40 +2,8 @@
 include 'global/config.php';
 include 'global/conexion.php';
 include 'carrito.php';
+include 'templates/cabecera.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bellesa-products</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-
-<body>
-
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-        <a class="navbar-brand" href="index.php">logo de la tienda</a>
-        <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div id="my-nav" class="collapse navbar-collapse">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Carrito(0)</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-    <br />
-    <br />
-    <div class="container">
         <br>
         <div class="alert alert-success">
           <?php echo ($mensaje); ?>
@@ -59,10 +27,10 @@ include 'carrito.php';
                             <p class="card-text">descripcion</p>
 
                             <form action="" method="post">
-                                <input type="text" name="id" id="id" value="<?php echo openssl_encrypt($producto['ID'],COD,KEY);?>">
-                                <input type="text" name="nombre" id="nombre" value="<?php echo openssl_encrypt($producto['Nombre'],COD,KEY); ?>">
-                                <input type="text" name="precio" id="precio" value="<?php echo openssl_encrypt($producto['Precio'],COD,KEY); ?>">
-                                <input type="text" name="cantidad" id="cantidad" value="<?php echo openssl_encrypt($producto['1'],COD,KEY);?>">
+                                <input type="hidden" name="id" id="id" value="<?php echo openssl_encrypt($producto['ID'],COD,KEY);?>">
+                                <input type="hidden" name="nombre" id="nombre" value="<?php echo openssl_encrypt($producto['Nombre'],COD,KEY); ?>">
+                                <input type="hidden" name="precio" id="precio" value="<?php echo openssl_encrypt($producto['Precio'],COD,KEY); ?>">
+                                <input type="hidden" name="cantidad" id="cantidad" value="<?php echo openssl_encrypt(1,COD,KEY);?>">
 
                             <button class="btn btn-primary" name="btnAccion" value="Agregar" type="submit">Agregar al carrito
                             </button>
@@ -81,6 +49,5 @@ include 'carrito.php';
         const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
         const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
     </script>
-</body>
-
-</html>
+<?php
+include 'templates/pie.php';?>
